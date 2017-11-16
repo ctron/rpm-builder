c4ec2b65d8d312c515b91f709317b92cbebf9453
Jens Reimann <ctron@dentrassi.de>
2017-11-16 13:26:17 +0100

    Add new configuration file flags

--- src/main/java/de/dentrassi/rpm/builder/EntryDetails.java
+++ src/main/java/de/dentrassi/rpm/builder/EntryDetails.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2016, 2017 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,9 +7,13 @@
  *
  * Contributors:
  *     IBH SYSTEMS GmbH - initial API and implementation
+ *     Red Hat Inc - add new file flags
  *******************************************************************************/
 package de.dentrassi.rpm.builder;
 
+import java.util.EnumSet;
+
+import org.eclipse.packagedrone.utils.rpm.FileFlags;
 import org.eclipse.packagedrone.utils.rpm.build.FileInformation;
 
 public class EntryDetails
@@ -17,6 +21,18 @@ public class EntryDetails
     private Short mode;
 
     private Boolean configuration;
+
+    private Boolean documentation;
+
+    private Boolean license;
+
+    private Boolean readme;
+
+    private Boolean ghost;
+
+    private Boolean missingOk;
+
+    private Boolean noreplace;
 
     private String user;
 
@@ -40,6 +56,66 @@ public class EntryDetails
     public Boolean getConfiguration ()
     {
         return this.configuration;
+    }
+
+    public void setDocumentation ( final Boolean documentation )
+    {
+        this.documentation = documentation;
+    }
+
+    public Boolean getDocumentation ()
+    {
+        return this.documentation;
+    }
+
+    public void setLicense ( final Boolean license )
+    {
+        this.license = license;
+    }
+
+    public Boolean getLicense ()
+    {
+        return this.license;
+    }
+
+    public void setReadme ( final Boolean readme )
+    {
+        this.readme = readme;
+    }
+
+    public Boolean getReadme ()
+    {
+        return this.readme;
+    }
+
+    public void setGhost ( final Boolean ghost )
+    {
+        this.ghost = ghost;
+    }
+
+    public Boolean getGhost ()
+    {
+        return this.ghost;
+    }
+
+    public void setMissingOk ( final Boolean missingOk )
+    {
+        this.missingOk = missingOk;
+    }
+
+    public Boolean getMissingOk ()
+    {
+        return this.missingOk;
+    }
+
+    public void setNoreplace ( final Boolean noreplace )
+    {
+        this.noreplace = noreplace;
+    }
+
+    public Boolean getNoreplace ()
+    {
+        return this.noreplace;
     }
 
     public void setUser ( final String user )
@@ -72,7 +148,37 @@ public class EntryDetails
 
         if ( this.configuration != null )
         {
-            info.setConfiguration ( this.configuration );
+            setFlag ( info, FileFlags.CONFIGURATION );
+            didApply = true;
+        }
+        if ( this.documentation != null )
+        {
+            setFlag ( info, FileFlags.DOC );
+            didApply = true;
+        }
+        if ( this.license != null )
+        {
+            setFlag ( info, FileFlags.LICENSE );
+            didApply = true;
+        }
+        if ( this.readme != null )
+        {
+            setFlag ( info, FileFlags.README );
+            didApply = true;
+        }
+        if ( this.ghost != null )
+        {
+            setFlag ( info, FileFlags.GHOST );
+            didApply = true;
+        }
+        if ( this.missingOk != null )
+        {
+            setFlag ( info, FileFlags.MISSINGOK );
+            didApply = true;
+        }
+        if ( this.noreplace != null )
+        {
+            setFlag ( info, FileFlags.NOREPLACE );
             didApply = true;
         }
         if ( this.user != null && !this.user.isEmpty () )
@@ -91,6 +197,20 @@ public class EntryDetails
             didApply = true;
         }
         return didApply;
+    }
+
+    private void setFlag ( final FileInformation info, final FileFlags flag )
+    {
+        EnumSet<FileFlags> flags = info.getFileFlags ();
+        if ( flags == null )
+        {
+            flags = EnumSet.of ( flag );
+            info.setFileFlags ( flags );
+        }
+        else
+        {
+            flags.add ( flag );
+        }
     }
 
 }
