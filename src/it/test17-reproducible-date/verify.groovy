3e8cf432ca422cb78ae18ddd29524de6099bbfa7
Jens Reimann <jreimann@redhat.com>
2022-08-19 12:26:56 +0200

    test: show expected and actual md5 sum

--- src/it/test17-reproducible-date/verify.groovy
+++ src/it/test17-reproducible-date/verify.groovy
@@ -1,12 +1,12 @@
 def generateMD5(File file) {
     def digest = java.security.MessageDigest.getInstance("MD5")
-    file.eachByte( 4096 ) { buffer, length ->
-        digest.update( buffer, 0, length )
+    file.eachByte(4096) { buffer, length ->
+        digest.update(buffer, 0, length)
     }
     digest.digest().encodeHex() as String
 }
 
-def verify ( ) {
+def verify() {
     Process proc = ("rpm -qilpv --dump " + basedir + "/target/test17-1.0.0-0.200901011100.noarch.rpm").execute()
     return proc.in.getText().trim()
 }
@@ -14,9 +14,10 @@ def verify ( ) {
 def result = verify()
 println "Verify: " + result
 
-def md5sum = generateMD5 ( new File ( basedir, "target/test17-1.0.0-0.200901011100.noarch.rpm" ) )
-if ( md5sum != "a4ce449e0e1b7b9045a6750f7a46301f" ) {
-    System.out.println ( "RPM MD5 doesn't match: " + md5sum );
+def expectedMd5Sum = "a4ce449e0e1b7b9045a6750f7a46301f";
+def md5sum = generateMD5(new File(basedir, "target/test17-1.0.0-0.200901011100.noarch.rpm"))
+if (md5sum != expectedMd5Sum) {
+    System.out.format("RPM MD5 doesn't match -  actual: %s, expected: %s%n", md5sum, expectedMd5Sum);
     return false;
 }
 
