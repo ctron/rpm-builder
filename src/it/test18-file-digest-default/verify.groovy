959be9e36cc22e20c8c8697e1beae73b70cc00cf
Jens Reimann <jreimann@redhat.com>
2023-01-11 10:49:32 +0100

    test: add new integration tests for file digest override

---
+++ src/it/test18-file-digest-default/verify.groovy
@@ -0,0 +1,16 @@
+def dump ( ) {
+	Process proc = ('rpm -q --dump -p ' + basedir + "/target/test18.rpm").execute()
+	return proc.in.getText().trim()
+}
+
+def actual = dump()
+println "Dump: " + actual
+
+def expected = "/etc/test.txt 11 1230807600 a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e 0100600 root root 0 0 0 X"
+
+if (actual != expected) {
+	System.out.format("RPM dump doesn't match -  actual: %s, expected: %s%n", actual, expected);
+	return false;
+}
+
+return true;
