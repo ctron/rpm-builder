6934e28d601ab636c7566a7b758735a95a646fc5
Jens Reimann <jreimann@redhat.com>
2023-01-11 09:46:48 +0100

    feat: Default file digest algorithm to SHA-256 and allow to override
    This change allows to configure the digest algorithm used for files
    inside the RPM file. Previously this defaulted to MD5. It is now also
    possible to override this.
    
    BREAKING CHANGE: By default, this will now create RPMs with a file
    digest algorithm of SHA-256. It is possible to override this back to
    MD5.
    
    Closes: #70

--- src/it/test15-default/verify.groovy
+++ src/it/test15-default/verify.groovy
@@ -8,4 +8,5 @@ println "Verify: " + result
 
 def m1 = result =~ /MD5 digest\: OK/
 def m2 = result =~ /Header SHA1 digest\: OK/
-return m1.find() && m2.find()
+def m3 = result =~ /Payload SHA256 digest\: OK/
+return m1.find() && m2.find() && m3.find()
