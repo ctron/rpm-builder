1331698abbe4228d3ef66c57baf2aca569a0b1bf
Möser, Daniel <daniel.moeser@iosb.fraunhofer.de>
2024-03-27 13:26:34 +0100

    Fix wrong ordering of files in rpm when using feature to generate intermediate directories

--- src/it/test19-intermediate-directories/verify.groovy
+++ src/it/test19-intermediate-directories/verify.groovy
@@ -1,23 +1,24 @@
-def dump() {
-    Process proc = ('rpm -q --dump -p ' + basedir + "/target/*.rpm").execute()
+def verifyFileInodes() {
+    Process proc = ['rpm', '-q', '--queryformat', '[%{FILEINODES} %{FILEMODES:perms} %-13{FILEUSERNAME} %-14{FILEGROUPNAME} %{FILENAMES}\n]', basedir.path + '/target/*.rpm'].execute() | 'sort -n'.execute()
+    proc.waitFor()
     return proc.in.getText().trim()
 }
 
-def actual = dump()
-println "Dump:\n" + actual
+def actual = verifyFileInodes()
+println "Verify file inodes:\n" + actual
 
 def expected = """\
-    /etc/mycompany/myapp 0 1230807600 0000000000000000000000000000000000000000000000000000000000000000 040755 root root 0 0 0 X
-    /etc/mycompany/myapp/defaults 0 1230807600 0000000000000000000000000000000000000000000000000000000000000000 040755 root root 0 0 0 X
-    /opt/mycompany/myapp 0 1230807600 0000000000000000000000000000000000000000000000000000000000000000 040777 myuser mygroup 0 0 0 X
-    /opt/mycompany/myapp/a 0 1230807600 0000000000000000000000000000000000000000000000000000000000000000 040777 myuser mygroup 0 0 0 X
-    /opt/mycompany/myapp/a/b 0 1230807600 0000000000000000000000000000000000000000000000000000000000000000 040777 myuser mygroup 0 0 0 X
-    /opt/mycompany/myapp/a/b/c 0 1230807600 0000000000000000000000000000000000000000000000000000000000000000 040777 myuser mygroup 0 0 0 X
-    /opt/mycompany/myapp/a/b/c/foobar 0 1230807600 e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 0100555 myuser mygroup 0 0 0 X
-    /opt/mycompany/otherapp/a/b/c/foobar 0 1230807600 e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 0100111 mygeneraluser mygeneralgroup 0 0 0 X""".stripIndent()
+    1 drwxrwxrwx myuser        mygroup        /opt/mycompany/myapp
+    2 drwxrwxrwx myuser        mygroup        /opt/mycompany/myapp/a
+    3 drwxrwxrwx myuser        mygroup        /opt/mycompany/myapp/a/b
+    4 drwxrwxrwx myuser        mygroup        /opt/mycompany/myapp/a/b/c
+    5 -r-xr-xr-x myuser        mygroup        /opt/mycompany/myapp/a/b/c/foobar
+    6 drwxr-xr-x root          root           /etc/mycompany/myapp
+    7 drwxr-xr-x root          root           /etc/mycompany/myapp/defaults
+    8 ---x--x--x mygeneraluser mygeneralgroup /opt/mycompany/otherapp/a/b/c/foobar""".stripIndent()
 
 if (actual != expected) {
-    System.out.format("RPM dump doesn't match - actual:%n%s%nexpected:%n%s%n", actual, expected);
+    System.out.format("RPM file inodes doesn't match - actual:%n%s%nexpected:%n%s%n", actual, expected);
     return false;
 }
 
