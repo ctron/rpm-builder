30ee4249b5e874a1285c5b054ddfc5b221138cfd
David Walluck <dwalluck@redhat.com>
2024-04-16 16:13:47 -0400

    Fix incorrect search string in test7

--- src/it/test7-srcpkg2/verify.groovy
+++ src/it/test7-srcpkg2/verify.groovy
@@ -1,8 +1,8 @@
 
-Process rpm = ("rpm -qip " + basedir + "/target/test7.rpm").execute();
+Process rpm = ("rpm -qip " + basedir.toString().replace("\\", "/") + "/target/test7.rpm").execute();
 
 def entries = rpm.in.readLines();
 
 println entries;
 
-return !entries.find { it == "foo.bar" };
+return entries.find { it =~ /Source RPM : foo\.bar/ };
